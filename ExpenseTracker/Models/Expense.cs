0d3473996f9f602ee5a3f3c0e515afdd61ab945f
yash-bhanderi <yash.bhanderi@marutitech.com>
2025-01-03 17:53:33 +0530

    Updated expenses

--- ExpenseTracker/Models/Expense.cs
+++ ExpenseTracker/Models/Expense.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public decimal Amount { get; set; }
-        public DateTime Date { get; set; }
+        public string Date { get; set; }
         public string Description { get; set; }
 
         // Foreign key for User
